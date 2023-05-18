ee769e50ab7049813915823f162b5a477c3dad90
buddhimac111 <91274532+buddhimac111@users.noreply.github.com>
2023-05-18 11:38:55 +0530

    created results page

---
+++ lib/results.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ResultsPage extends StatefulWidget {
+  const ResultsPage({Key? key}) : super(key: key);
+
+  @override
+  State<ResultsPage> createState() => _ResultsPageState();
+}
+
+class _ResultsPageState extends State<ResultsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
